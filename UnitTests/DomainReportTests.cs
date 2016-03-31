6f29d14effe36e30c41f90c0a3e2ca32d408bd82
alleeclark <allee.clark@bison.howard.edu>
2016-03-31 08:38:26 -0400

    need to add a file

---
+++ UnitTests/DomainReportTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirusTotalNET;
+using VirusTotalNET.Objects;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DomainReportTests
+    {
+        private static VirusTotal _virusTotal;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _virusTotal = new VirusTotal(ConfigurationManager.AppSettings["ApiKey"]);
+        }
+
+        [TestMethod]
+        public void GetDomainReportKnownDomain()
+        {
+            DomainReport report = _virusTotal.GetDomainReport("google.com");
+            Assert.AreEqual(ReportResponseCode.Present, report.ResponseCode);
+        }
+
+        [TestMethod]
+        public void GetDomainReportUnknownDomain()
+        {
+            DomainReport report = _virusTotal.GetDomainReport(Guid.NewGuid() + ".com");
+            Assert.AreEqual(ReportResponseCode.NotPresent, report.ResponseCode);
+        }
+
+        [TestMethod]
+        public void GetDomainReportInvalidDomain()
+        {
+            //This test should pass, but VT have added "." as a domain, which should not happen.
+            //It therefore fails, which is to be expcted until VT team fixes this bug.
+            DomainReport report = _virusTotal.GetDomainReport(".");
+            Assert.AreEqual(ReportResponseCode.Error, report.ResponseCode);
+        }
+    }
+}
---
+++ VirusTotal.NET Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using VirusTotalNET;
+using VirusTotalNET.Objects;
+
+namespace VirusTotalNETClient
+{
+    class Program
+    {
+        private const string ScanUrl = "http://www.google.com/";
+
+        static void Main(string[] args)
+        {
+            VirusTotal virusTotal = new VirusTotal(ConfigurationManager.AppSettings["ApiKey"]);
+
+            //Use HTTPS instead of HTTP
+            virusTotal.UseTLS = true;
+
+            //Create the EICAR test virus. See http://www.eicar.org/86-0-Intended-use.html
+            FileInfo fileInfo = new FileInfo("EICAR.txt");
+            File.WriteAllText(fileInfo.FullName, @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
+
+            //Check if the file has been scanned before.
+            FileReport fileReport = virusTotal.GetFileReport(fileInfo);
+
+            bool hasFileBeenScannedBefore = fileReport.ResponseCode == ReportResponseCode.Present;
+
+            Console.WriteLine("File has been scanned before: " + (hasFileBeenScannedBefore ? "Yes" : "No"));
+
+            //If the file has been scanned before, the results are embedded inside the report.
+            if (hasFileBeenScannedBefore)
+            {
+                PrintScan(fileReport);
+            }
+            else
+            {
+                ScanResult fileResult = virusTotal.ScanFile(fileInfo);
+                PrintScan(fileResult);
+            }
+
+            Console.WriteLine();
+
+            UrlReport urlReport = virusTotal.GetUrlReport(ScanUrl);
+
+            bool hasUrlBeenScannedBefore = urlReport.ResponseCode == ReportResponseCode.Present;
+            Console.WriteLine("URL has been scanned before: " + (hasUrlBeenScannedBefore ? "Yes" : "No"));
+
+            //If the url has been scanned before, the results are embedded inside the report.
+            if (hasUrlBeenScannedBefore)
+            {
+                PrintScan(urlReport);
+            }
+            else
+            {
+                ScanResult urlResult = virusTotal.ScanUrl(ScanUrl);
+                PrintScan(urlResult);
+            }
+
+            Console.WriteLine("Press a key to continue");
+            Console.ReadLine();
+        }
+
+        private static void PrintScan(ScanResult scanResult)
+        {
+            Console.WriteLine("Scan ID: " + scanResult.ScanId);
+            Console.WriteLine("Message: " + scanResult.VerboseMsg);
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(FileReport fileReport)
+        {
+            Console.WriteLine("Scan ID: " + fileReport.ScanId);
+            Console.WriteLine("Message: " + fileReport.VerboseMsg);
+
+            if (fileReport.ResponseCode == ReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(UrlReport urlReport)
+        {
+            Console.WriteLine("Scan ID: " + urlReport.ScanId);
+            Console.WriteLine("Message: " + urlReport.VerboseMsg);
+
+            if (urlReport.ResponseCode == ReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, ScanEngine> scan in urlReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
